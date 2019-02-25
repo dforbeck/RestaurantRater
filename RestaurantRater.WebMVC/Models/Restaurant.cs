5b9886a5a42e08a7941c70cf4e1c9c02d58b54e3
Debbie <debbieforbeck@gmail.com>
2019-02-25 14:45:50 -0500

    Added Restaurant class and established dbset/connection string

---
+++ RestaurantRater.WebMVC/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.WebMVC.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+
+        public class RestaurantDBContext : DbContext //snapshot of the dataset
+        {
+            public DbSet<Restaurant>Restaurants { get; set; } //defining the dbcontext referene type
+
+        }
+    }
+}
